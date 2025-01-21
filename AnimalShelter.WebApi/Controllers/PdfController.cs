1e33ee5cde239550fb42106cd44dcc3b4659ce82
filip7557 <filip.curic@student.ferit.hr>
2025-01-21 13:52:44 +0100

    Add pdf controller and response

---
+++ AnimalShelter.WebApi/Controllers/PdfController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalShelter.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var fileName = "example.pdf";
+            var filePath = Path.Combine("wwwroot/files", fileName);
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound(new { message = "File not found." });
+            }
+
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/pdf", fileName);
+        }
+    }
+}
