b59e6bcaf7b49c90b2aff9f54cae46196e676ff6
filip7557 <filip.curic@student.ferit.hr>
2025-01-23 13:04:18 +0100

    Add breed property

--- AnimalShelter.WebApi/Models/Dog.cs
+++ AnimalShelter.WebApi/Models/Dog.cs
@@ -6,14 +6,17 @@
         {
         }
 
-        public Dog(string name, int age)
+        public Dog(string name, int age, Guid breedId)
         {
             Name = name;
             Age = age;
+            BreedId = breedId;
         }
 
         public Guid Id { get; set; }
         public required string Name { get; set; }
+        public Guid? BreedId { get; set; }
+        public Breed? Breed { get; set; }
         public int Age { get; set; }
     }
 }
